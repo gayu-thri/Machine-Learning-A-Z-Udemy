a04f2f690b5bf04df067c1b19fcc988ae1a89714
gayu-thri <egayu3@gmail.com>
2020-03-07 16:31:00 +0530

    Updated

--- Part 1 - Data Preprocessing/categorical_data.py
+++ Part 1 - Data Preprocessing/categorical_data.py
@@ -6,15 +6,15 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('Data.csv')
+dataset = pd.read_csv(r'D:\Github\Machine-Learning-A-Z-Udemy\Part 1 - Data Preprocessing\Data.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 3].values
 
 # Taking care of missing data
-from sklearn.preprocessing import Imputer
-imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
-imputer = imputer.fit(X[:, 1:3])
-X[:, 1:3] = imputer.transform(X[:, 1:3])
+from sklearn.impute import SimpleImputer
+missingvalues = SimpleImputer(missing_values = np.nan, strategy = 'mean', verbose = 0)
+missingvalues = missingvalues.fit(X[:, 1:3])
+X[:, 1:3]=missingvalues.transform(X[:, 1:3])
 
 # Encoding categorical data
 # Encoding the Independent Variable
