b73d78bcbc0fa05c9dd302e463763dd61a38412a
gayu-thri <egayu3@gmail.com>
2020-03-30 13:00:12 +0530

    Replaced classes of the updated version

--- Part 1 - Data Preprocessing/categorical_data.py
+++ Part 1 - Data Preprocessing/categorical_data.py
@@ -7,22 +7,43 @@ import pandas as pd
 
 # Importing the dataset
 dataset = pd.read_csv(r'D:\Github\Machine-Learning-A-Z-Udemy\Part 1 - Data Preprocessing\Data.csv')
-X = dataset.iloc[:, :-1].values   '''all rows;all columns except last one ~ dependent attributes'''
-y = dataset.iloc[:, 3].values   '''last column alone ~ independent attribute'''
+
+''' all rows;all columns except last one ~ dependent attributes '''
+X = dataset.iloc[:, :-1].values   
+''' last column alone ~ independent attribute '''
+y = dataset.iloc[:, 3].values   
 
 # Taking care of missing data
 from sklearn.impute import SimpleImputer
 missingvalues = SimpleImputer(missing_values = np.nan, strategy = 'mean', verbose = 0)  #‘NaN’ was replaced by ‘np.nan’
-missingvalues = missingvalues.fit(X[:, 1:3])
+
+''' columns with missing data '''
+missingvalues = missingvalues.fit(X[:, 1:3])    
+'''1:3 => 3 not included'''
+''' replaces missing data with mean '''
 X[:, 1:3]=missingvalues.transform(X[:, 1:3])
 
 # Encoding categorical data
 # Encoding the Independent Variable
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+ct = ColumnTransformer([('encoder', OneHotEncoder(), [0])], remainder='passthrough')
+X = np.array(ct.fit_transform(X), dtype=np.float)
+# Encoding Y data
+from sklearn.preprocessing import LabelEncoder
+y = LabelEncoder().fit_transform(y)
+
+'''
+#OLDER VERSION
+# Encoding categorical data
+# Encoding the Independent Variable
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder_X = LabelEncoder()
+#takes all entries of first column
 X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
 onehotencoder = OneHotEncoder(categorical_features = [0])
 X = onehotencoder.fit_transform(X).toarray()
 # Encoding the Dependent Variable
 labelencoder_y = LabelEncoder()
 y = labelencoder_y.fit_transform(y)
+'''
