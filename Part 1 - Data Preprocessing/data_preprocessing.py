e6edc393d14e3b7177efa8f94c7ecdd6d51cd787
gayu-thri <egayu3@gmail.com>
2020-03-31 15:21:56 +0530

    Create data train and test split

--- Part 1 - Data Preprocessing/data_preprocessing.py
+++ Part 1 - Data Preprocessing/data_preprocessing.py
@@ -7,10 +7,13 @@ import pandas as pd #for import/management of datasets
 
 # Importing the dataset
 dataset = pd.read_csv(r'D:\Udemy Machine Learning A-Z\Part 1 - Data Preprocessing\Data.csv')
-        #r to produce a raw string 
+        #r to produce a raw string
+        
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 3].values
+
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
